ce121ef141ca44ff6a6d6e35e0ae09e252d55060
telegeyN <ntelegey@gmail.com>
2024-02-16 02:27:47 +0600

    Initial Commit

---
+++ 4hw2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4hw2
+//
+//  Created by Telegey Nurbekova on 16/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
