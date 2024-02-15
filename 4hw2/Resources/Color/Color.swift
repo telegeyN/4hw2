0f424c03d46f139a07cff3d886391cf023747fa9
telegeyN <ntelegey@gmail.com>
2024-02-16 05:43:17 +0600

    Сделала три экрана: 1) Sign Up page; 2) Forgot Password page; 3) New Password Page Каждая кнопочка рабочая и все везде проверяется, прежде чем перейти на другой экран Использовала функции для создания элементов, чтобы не засорять код

---
+++ 4hw2/Resources/Color/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  4hw2
+//
+//  Created by Telegey Nurbekova on 16/02/24.
+//
+
+import Foundation
---
+++ 4hw2/Resources/Fonts/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  4hw2
+//
+//  Created by Telegey Nurbekova on 16/02/24.
+//
+
+import Foundation
